fab8c855cf57c0820aab586f9ce3b939173d7c70
angellee <angelriapurnamasari17@gmail.com>
2019-06-14 16:15:08 +0700

    add Edit Action with Test

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -12,6 +12,6 @@ class Chef < ApplicationRecord
     validates :email, presence: {messages:'Email cannot be blank!'},
     format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i},
     uniqueness: { case_sensitive: false }
-    validates :password, presence: true, length: { minimum: 5 }
+    validates :password, presence: true, length: { minimum: 5 }, allow_nil: true #allow us to not change our password
 
 end
